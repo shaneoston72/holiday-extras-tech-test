c805ac9fd497d4c21c8e262d295de986278408b2
Shane Oston Stowe <shaneoston@shaneoston.com>
2016-06-01 17:00:43 +0100

    added app template to app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,6 +5,23 @@ import { Observable } from 'rxjs/Rx';
 @Component({
   selector: 'he-app',
   template:`
+    <div class="main-container">
+      <h1>Holiday Extras Tech Test</h1>
+      <p>submitted by Shane Oston Stowe</p>
+      <div class="flex-container">
+        <div *ngFor="let flickrItem of flickrItems" [flickrItem]="flickrItem">
+          <img src="{{ flickrItem.imgUrl }}" alt="" className="flickr-photo"/>
+          <div class="flickr-details">
+            <h3 class="flickr-title">{{ flickrItem.photoTitle }}</h3>
+            <p>
+              <span class="smallType">by </span>
+              <span class="flickr-author"> {{ flickrItem.photoAuthor }}</span></p>
+              <div class="flickr-description" [innerHTML]="flickrItem.photoDescription"></div>
+              <p class="flickr-tags">Tags: {{ flickrItem.photoTags }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   `
 })
 export class AppComponent {
