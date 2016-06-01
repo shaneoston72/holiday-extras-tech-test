d7cb355ea9198e9809e859c2a9c073f612833c15
Shane Oston Stowe <shaneoston@shaneoston.com>
2016-06-01 16:57:48 +0100

    configured HTTP request to flickr and created method to alter the response to only have items

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,4 +11,24 @@ export class AppComponent {
   public items;
 
   constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.getItems();
+  }
+
+  getItems() {
+    this.http.get('https://api.flickr.com/services/feeds/photos_public.gne?format=json')
+      .subscribe((res: Response) => {
+        this.data = res._body;
+        this.result = this.transformRes(this.data);
+      });
+  }
+
+  transformRes(data) {
+    data = data.replace('jsonFlickrFeed(','');
+    data = data.replace('})', '}');
+    data = data.replace(/\\'/g, "'");
+    data = JSON.parse(data);
+    return data.items;
+  }
 }
